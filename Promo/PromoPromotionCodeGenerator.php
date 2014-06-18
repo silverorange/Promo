95e56d29519010d90541bcc6737dbbdb9854fe43
Isaac Grant <isaac@silverorange.com>
2014-06-18 12:19:21 -0300

    Move promotion code generation into a reusable object.

---
+++ Promo/PromoPromotionCodeGenerator.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once 'Text/Password.php';
+require_once 'Site/SiteApplication.php';
+require_once 'SwatDB/SwatDB.php';
+
+/**
+ * @package   Promo
+ * @copyright 2011-2014 silverorange
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+class PromoPromotionCodeGenerator
+{
+	// {{{ class constants
+
+	const LENGTH = 8;
+
+	// }}}
+	// {{{ protected properties
+
+	/**
+	 * @var SiteApplication
+	 */
+	protected $app;
+
+	// }}}
+	// {{{ public function __construct()
+
+	public function __construct(SiteApplication $app)
+	{
+		$this->app = $app;
+	}
+
+	// }}}
+	// {{{ public function getCodes()
+
+	public function getCodes(PromoPromotion $promotion, $quantity, $prefix = '')
+	{
+		$prefix = strtolower($prefix);
+
+		$codes = Text_Password::createMultiple(
+			$quantity,
+			self::LENGTH
+		);
+
+		$codes_out = array();
+		foreach ($codes as $code) {
+			if ($prefix != '') {
+				$code = sprintf('%s-%s', $prefix, $code);
+			}
+			$codes_out[] = $code;
+		}
+
+		return $this->verifyUniqueness($promotion, $codes_out, $prefix);
+	}
+
+	// }}}
+	// {{{ protected function verifyUniqueness()
+
+	protected function verifyUniqueness(PromoPromotion $promotion, $codes,
+		$prefix = null)
+	{
+		$lower_codes = array_map('strtolower', $codes);
+
+		$instance_where = ($promotion->instance instanceof SiteInstance)
+			? sprintf(
+				'Promotion.instance = %s',
+				$this->app->db->quote(
+					$promotion->instance->id,
+					'integer'
+				)
+			)
+			: '1 = 1';
+
+		$sql = sprintf(
+			'select PromotionCode.code
+				from PromotionCode
+				inner join Promotion on PromotionCode.promotion = Promotion.id
+				where lower(PromotionCode.code) in (%s) and %s',
+			$this->app->db->datatype->implodeArray($lower_codes, 'text'),
+			$instance_where
+		);
+
+		$existing_codes = SwatDB::query($this->app->db, $sql);
+
+		$new_codes = array();
+		$old_codes = array();
+
+		foreach ($existing_codes as $existing_code) {
+			$new_code = $this->generateCode($prefix);
+			// Make sure the new code is unique for any new generated codes as
+			// well.
+			while (in_array($new_code, $codes) ||
+				in_array($new_code, $new_codes)) {
+				$new_code = $this->generateCode($prefix);
+			}
+
+			$new_codes[] = $new_code;
+			$old_codes[] = $existing_code->code;
+		}
+
+		$codes = array_diff($codes, $old_codes);
+		$codes = array_merge($codes, $new_codes);
+
+		if (count($new_codes) > 0) {
+			$codes = $this->verifyUniqueness($codes, $prefix);
+		}
+
+		return $codes;
+	}
+
+	// }}}
+	// {{{ protected function generateCode()
+
+	protected function generateCode($prefix = null)
+	{
+		$code = Text_Password::create(self::LENGTH);
+		if ($prefix != '') {
+			$code = sprintf('%s-%s', $prefix, $code);
+		}
+
+		return $code;
+	}
+
+	// }}}
+}
+
+?>
--- Promo/admin/components/Promotion/GenerateCodes.php
+++ Promo/admin/components/Promotion/GenerateCodes.php
@@ -5,7 +5,7 @@ require_once 'Admin/exceptions/AdminNotFoundException.php';
 require_once 'SwatDB/SwatDB.php';
 require_once 'SwatI18N/SwatI18NLocale.php';
 require_once 'Swat/SwatMessage.php';
-require_once 'Text/Password.php';
+require_once 'Promo/PromoPromotionCodeGenerator.php';
 require_once 'Promo/dataobjects/PromoPromotion.php';
 require_once 'Promo/dataobjects/PromoPromotionCode.php';
 
@@ -17,11 +17,6 @@ require_once 'Promo/dataobjects/PromoPromotionCode.php';
  */
 class PromoPromotionGenerateCodes extends AdminDBEdit
 {
-	// {{{ class constants
-
-	const LENGTH = 8;
-
-	// }}}
 	// {{{ protected properties
 
 	/**
@@ -115,20 +110,12 @@ class PromoPromotionGenerateCodes extends AdminDBEdit
 			$this->app->db->quote($now, 'date')
 		);
 
-		$codes = Text_Password::createMultiple(
+		$generator = $this->getPromotionCodeGenerator();
+		$codes = $generator->getCodes(
+			$this->promotion,
 			$values['quantity'],
-			self::LENGTH
+			$values['prefix']
 		);
-
-		$codes_out = array();
-		foreach ($codes as $code) {
-			if ($prefix != '') {
-				$code = sprintf('%s-%s', $prefix, $code);
-			}
-			$codes_out[] = $code;
-		}
-
-		$codes = $this->verifyUniqueness($codes_out, $prefix);
 
 		// insert values
 		$values_out = array();
@@ -166,70 +153,11 @@ class PromoPromotionGenerateCodes extends AdminDBEdit
 	}
 
 	// }}}
-	// {{{ protected function verifyUniqueness()
+	// {{{ protected function getPromotionCodeGenerator()
 
-	protected function verifyUniqueness($codes, $prefix = null)
+	protected function getPromotionCodeGenerator()
 	{
-		$lower_codes = array_map('strtolower', $codes);
-
-		$instance_where = ($this->promotion->instance instanceof SiteInstance)
-			? sprintf(
-				'Promotion.instance = %s',
-				$this->app->db->quote(
-					$this->promotion->instance->id,
-					'integer'
-				)
-			)
-			: '1 = 1';
-
-		$sql = sprintf(
-			'select PromotionCode.code
-				from PromotionCode
-				inner join Promotion on PromotionCode.promotion = Promotion.id
-				where lower(PromotionCode.code) in (%s) and %s',
-			$this->app->db->datatype->implodeArray($lower_codes, 'text'),
-			$instance_where
-		);
-
-		$existing_codes = SwatDB::query($this->app->db, $sql);
-
-		$new_codes = array();
-		$old_codes = array();
-
-		foreach ($existing_codes as $existing_code) {
-			$new_code = $this->generateCode($prefix);
-			// Make sure the new code is unique for any new generated codes as
-			// well.
-			while (in_array($new_code, $codes) ||
-				in_array($new_code, $new_codes)) {
-				$new_code = $this->generateCode($prefix);
-			}
-
-			$new_codes[] = $new_code;
-			$old_codes[] = $existing_code->code;
-		}
-
-		$codes = array_diff($codes, $old_codes);
-		$codes = array_merge($codes, $new_codes);
-
-		if (count($new_codes) > 0) {
-			$codes = $this->verifyUniqueness($codes, $prefix);
-		}
-
-		return $codes;
-	}
-
-	// }}}
-	// {{{ protected function generateCode()
-
-	protected function generateCode($prefix = null)
-	{
-		$code = Text_Password::create(self::LENGTH);
-		if ($prefix != '') {
-			$code = sprintf('%s-%s', $prefix, $code);
-		}
-
-		return $code;
+		return new PromoPromotionCodeGenerator($this->app);
 	}
 
 	// }}}
